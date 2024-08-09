19897d050a01da77c422ba38fe3d6a2dee29172f
saiparameshwarrao <134683789+saiparameshwarrao@users.noreply.github.com>
2024-08-09 19:13:46 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+def package_tracking_sort(arr):
+    l = len(arr)
+    for i in range(l-1):
+        min_index = i
+        for j in range(i+1, l):
+            if arr[j] < arr[min_index]:
+                min_index = j
+            if arr[j] > arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    print("Sorted tracking numbers:", arr)
+    
+if __name__ == '__main__':
+    tracking_array = []
+    num = int(input("Enter number of traking numbers: "))
+    for _ in range(num):
+        val = int(input("Enter Package Tracking Number: "))
+        tracking_array.append(val)
+    package_tracking_sort(tracking_array)
