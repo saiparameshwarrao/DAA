9ded7c7096e8d253ba6b9edb95e3a5be2ebd0388
saiparameshwarrao <134683789+saiparameshwarrao@users.noreply.github.com>
2024-08-11 12:15:46 +0530

    Update package_tracking_sort.py

--- package_tracking_sort.py
+++ package_tracking_sort.py
@@ -1,10 +1,17 @@
-def package_tracking_sort(arr):
-    l = len(arr)
+def package_tracking_sort(arr,l):
+    choice = int(input('Do you want asc(1) or desc(2): '))    
     for i in range(l-1):
         min_index = i
         for j in range(i+1, l):
-            if arr[j] < arr[min_index]:
-                min_index = j
+            if choice == 1:
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            elif choice == 2:
+                if arr[j] > arr[min_index]:
+                    min_index = j
+            else:
+                print("Invalid choice. Please enter 1 for ascending or 2 for descending.")
+                return
         arr[i], arr[min_index] = arr[min_index], arr[i]
     
     print("Sorted tracking numbers:", arr)
@@ -15,4 +22,4 @@ if __name__ == '__main__':
     for _ in range(num):
         val = int(input("Enter Package Tracking Number: "))
         tracking_array.append(val)
-    package_tracking_sort(tracking_array)
+    package_tracking_sort(tracking_array,num)
